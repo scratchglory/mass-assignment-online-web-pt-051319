1b3a57141b32db4e7b07d7e73528fa086dff895b
Adella <adellakenn@gmail.com>
2019-06-14 15:12:05 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,8 +3,8 @@ class Person
                 :weight, :handed, :complexion, :t_shirt_size, 
                 :wrist_size, :glove_size, :pant_length, :pant_width
 
-  def initialize(attribute)
-    atttribute.each {|key, value| self.send(("#{key}="), value) }
+  def initialize(attributes)
+    atttributes.each {|key, value| self.send(("#{key}="), value) }
   end
 
 end
